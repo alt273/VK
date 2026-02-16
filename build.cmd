06eabc2850b98c68967b6aa5358cf0dd3dcf9d08
Денис <schastlivyy.2025@mail.ru>
2026-02-16 08:18:08 +0300

    Update build.cmd

--- build.cmd
+++ build.cmd
@@ -1,2 +1,2 @@
-
++@PowerShell
 PowerShell -NoProfile -NoLogo -ExecutionPolicy unrestricted -Command "[System.Threading.Thread]::CurrentThread.CurrentCulture = ''; [System.Threading.Thread]::CurrentThread.CurrentUICulture = '';& '%~dp0build.ps1' default-build %*; exit $LASTEXITCODE"
