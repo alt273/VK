5b71beba8154989f87abad61c4e5f7e20eae0d44
Денис <schastlivyy.2025@mail.ru>
2026-02-17 08:04:01 +0300

    Update build.cmd

--- build.cmd
+++ build.cmd
@@ -1,2 +1,3 @@
 +@PowerShell
 PowerShell -NoProfile -NoLogo -ExecutionPolicy unrestricted -Command "[System.Threading.Thread]::CurrentThread.CurrentCulture = ''; [System.Threading.Thread]::CurrentThread.CurrentUICulture = '';& '%~dp0build.ps1' default-build %*; exit $LASTEXITCODE"
+npmRegistryServer: "https://pkgs.dev.azure.com/dnceng/public/_packaging/dotnet-public-npm/npm/registry/"
